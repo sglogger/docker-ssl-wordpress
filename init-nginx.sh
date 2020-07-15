3ee40f95cb25a0b4ce09ec7a23503b1373a5ae86
Steven Glogger <steven@glogger.ch>
2020-07-15 15:49:40 +0200

    updates

--- init-nginx.sh
+++ init-nginx.sh
@@ -11,4 +11,4 @@ fi
 
 
 # search in nginx-conf/nginx.conf for WP_HOSTNAME and replace it
-sed -i nginx-conf/nginx.conf 's/WP_HOSTNAME/$WP_HOSTNAME/g'
+sed -i 's/WP_HOSTNAME/$WP_HOSTNAME/g' nginx-conf/nginx.conf
